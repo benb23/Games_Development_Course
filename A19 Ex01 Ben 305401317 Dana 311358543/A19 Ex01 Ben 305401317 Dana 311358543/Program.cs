89e8ca079a233e986d245bb057d0be27bb495a25
Ben Bublil <benbu@mta.ac.il>
2018-11-07 15:53:52 +0200

    EX1 project creation

---
+++ A19 Ex01 Ben 305401317 Dana 311358543/A19 Ex01 Ben 305401317 Dana 311358543/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace A19_Ex01_Ben_305401317_Dana_311358543
+{
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
