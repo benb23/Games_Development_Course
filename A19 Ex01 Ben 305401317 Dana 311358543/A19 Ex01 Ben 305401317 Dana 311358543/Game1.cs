9b68d721276909400cd3e19273681656a058ad7a
Ben Bublil <benbu@mta.ac.il>
2018-11-08 19:09:00 +0200

    Drawing Background and Ship

--- A19 Ex01 Ben 305401317 Dana 311358543/A19 Ex01 Ben 305401317 Dana 311358543/Game1.cs
+++ A19 Ex01 Ben 305401317 Dana 311358543/A19 Ex01 Ben 305401317 Dana 311358543/Game1.cs
@@ -12,10 +12,20 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D m_TextureBackground;
+        Texture2D m_TextureShip;
+
+        Vector2 m_PositionBackground;
+        Vector2 m_PositionShip;
+
+        Color m_TintBackground = Color.White;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            this.IsMouseVisible = true;
         }
 
         /// <summary>
@@ -24,12 +34,36 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        //protected override void Initialize()
+        //{
+        //    // TODO: Add your initialization logic here
+        //}
 
-            base.Initialize();
+
+        private void InitPositions()
+        {
+            // 1. init the ship position
+            // Get the bottom and left:
+            float x = 0;
+            float y = (float)GraphicsDevice.Viewport.Height;
+
+            // Offset for ship start point:
+            y -= (m_TextureShip.Height * 1.5f);
+
+            m_PositionShip = new Vector2(x, y);
+
+
+            // 2. Init the enemy position
+
+            // 3. Init the bg position:
+            m_PositionBackground = Vector2.Zero;
+
+            //create an alpah channel for background:
+            Vector4 bgTint = Vector4.One;
+            bgTint.W = 0.4f; // set the alpha component to 0.2
+            m_TintBackground = new Color(bgTint);
         }
+
 
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
@@ -39,7 +73,9 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            m_TextureBackground = Content.Load<Texture2D>(@"Sprites\BG_Space01_1024x768");
+            m_TextureShip = Content.Load<Texture2D>(@"Sprites\Ship01_32x32");
+            InitPositions();
             // TODO: use this.Content to load your game content here
         }
 
@@ -62,6 +98,9 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+
+            // clamp the position between screen boundries:
+           // m_PositionShip.X = MathHelper.Clamp(m_PositionShip.X, 0, this.GraphicsDevice.Viewport.Width - m_TextureShip.Width);
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -73,8 +112,12 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
+            spriteBatch.Begin();
+            spriteBatch.Draw(m_TextureBackground, m_PositionBackground, m_TintBackground); // tinting with alpha channel
+            spriteBatch.Draw(m_TextureShip, m_PositionShip, Color.White); //no tinting
+            spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
