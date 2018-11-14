6933a393ab3df597a2a4b7723d5074020b61c0f0
danakeydar <dana260893@gmail.com>
2018-11-14 13:43:59 +0200

    k

--- A19 Ex01 Ben 305401317 Dana 311358543/A19 Ex01 Ben 305401317 Dana 311358543/GameSprites/Bullet.cs
+++ A19 Ex01 Ben 305401317 Dana 311358543/A19 Ex01 Ben 305401317 Dana 311358543/GameSprites/Bullet.cs
@@ -12,27 +12,51 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
 {
     class Bullet : Sprite
     {
-        public enum BulletType { EnemyBullet, SpaceShipBullet };
-
+        public enum BulletType { SpaceShipBullet = -1, EnemyBullet = 1 };
         private BulletType m_Type;
         private readonly float r_BulletVelocity = 155;
 
         public override void Update(GameTime gameTime)
         {
-           if(isBulletHitElement())//collision 
+           if(isBulletHitElement()|| isBulletHitTheScreenBorder())//collision 
            {
                 //destroy element
+                RemoveComponent();
                 m_visible = false;
-           }
+            }
            else
            {
-                Position = new Vector2(Position.X,Position.Y + Direction*r_BulletVelocity* (float)gameTime.ElapsedGameTime.TotalSeconds);
+                Position = new Vector2(Position.X,Position.Y + (float)m_Type*r_BulletVelocity* (float)gameTime.ElapsedGameTime.TotalSeconds);
            }
         }
 
-        public Bullet(Game game, BulletType bulletType ) :base(game)
+        private bool isBulletHitTheScreenBorder()
+        {
+            if(m_Position.Y==0 || m_Position.Y==Game.GraphicsDevice.Viewport.Height)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Bullet(Game game, BulletType bulletType, Vector2 shooterPosition ) :base(game)
         {
             m_AssetName = @"Sprites\Bullet";
+            
+            if(m_Type == BulletType.EnemyBullet)
+            {
+                m_Tint = Color.Green;
+            }
+            else
+            {
+                m_Tint = Color.Red;
+            }
+
+            m_Position = shooterPosition;
+
+            m_visible = true;
             m_Type = bulletType;
         }
 
@@ -42,13 +66,9 @@ namespace A19_Ex01_Ben_305401317_Dana_311358543
             return false;
         }
 
-        public void initialize()
-        {
-
-        }
         public override void initPosition()
         {
-
+            Position=new Vector2(Position.X + 32/2, Position.Y +(float)m_Type*10);//TODO: CONST 32 SHOOTER WIDTH
         }
     }
 }
