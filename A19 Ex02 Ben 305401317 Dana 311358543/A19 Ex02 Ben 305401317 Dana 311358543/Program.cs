0d2d82f927f1050fc133f1a2b050c968abf7a6ad
Ben Bublil <benbu@mta.ac.il>
2018-12-31 14:28:16 +0200

    new ex2

---
+++ A19 Ex02 Ben 305401317 Dana 311358543/A19 Ex02 Ben 305401317 Dana 311358543/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A19_Ex02_Ben_305401317_Dana_311358543
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+#endif
+}
