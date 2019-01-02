41a8ec42bd7cdc4444e602cfcabb6fef119b9d7a
Ben Bublil <benbu@mta.ac.il>
2019-01-02 21:55:46 +0200

    WALL

---
+++ A19 Ex02 Ben 305401317 Dana 311358543/A19 Ex02 Ben 305401317 Dana 311358543/GameObject/WallsGroup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Infrastructure;
+
+namespace A19_Ex02_Ben_305401317_Dana_311358543
+{
+    public class WallsGroup : DrawableGameComponent
+    {
+        private const int k_NumOfWalls = 4;
+        private float m_Direction = 1f;
+        private List<Wall> m_WallGroup;
+        private Color[] m_OriginalPixels;
+
+        public WallsGroup(Game i_Game) : base(i_Game)
+        {
+            this.m_WallGroup = new List<Wall>(k_NumOfWalls);
+        }
+
+        public override void Initialize()
+        {
+            foreach (Wall wall in m_WallGroup)
+            {
+                
+            }
+
+            base.Initialize();
+        }
+
+        public override void Update(GameTime i_GameTime)
+        {
+            base.Update(i_GameTime);
+        }
+    }
+}
